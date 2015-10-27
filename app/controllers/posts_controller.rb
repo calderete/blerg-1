2ca3ff873b897a2604d13fee00700022795f6b1c
Charles <codprestige10@gmail.com>
2015-10-27 16:00:46 -0400

    getting something going

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,5 +19,26 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     render :show
   end
+
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
